401cf7e853ef7ba8349eb23763eea7d2f5a8da53
alim <wajihafatima689@gmail.com>
2024-11-05 18:38:22 +0500

    .

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
--- server/index.js
+++ server/index.js
@@ -12,6 +12,6 @@ connectDB();
 
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 module.exports = app;
