15c53c090964951fef95ccc228684339d20013ed
alim <wajihafatima689@gmail.com>
2024-11-05 19:08:01 +0500

    .check

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB connection error:", err));
 
